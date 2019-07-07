9027efb58228c460d100f8bae2a3210eb50cbbc6
wintonBy <winton_by@126.com>
2019-07-07 23:51:14 +0800

    Add ripple for item click

--- bottomnavigationview/src/main/java/com/winton/bottomnavigationview/NavigationItemView.java
+++ bottomnavigationview/src/main/java/com/winton/bottomnavigationview/NavigationItemView.java
@@ -2,6 +2,7 @@ package com.winton.bottomnavigationview;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.support.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -71,7 +72,7 @@ public class NavigationItemView extends RelativeLayout {
         init();
     }
 
-
+    @RequiresApi(21)
     private void init(){
         tvTitle = new TextView(getContext());
         initTitle();
@@ -84,6 +85,7 @@ public class NavigationItemView extends RelativeLayout {
         tvTitle.setFocusable(false);
         ivIcon.setFocusable(false);
         reminderView.setFocusable(false);
+        setBackground(getResources().getDrawable(R.drawable.item_bg,null));
     }
 
     /**
